1a46412269f956c45a3b28cf58553c18a4bf5868
若熙~ <oncwnuE9cvc3j9g4Yk0AFSNyIEF0@git.weixin.qq.com>
2024-03-17 14:16:35 +0800

    修改

--- api/v1/pay/pays.go
+++ api/v1/pay/pays.go
@@ -2,21 +2,20 @@ package pay
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
-    "github.com/flipped-aurora/gin-vue-admin/server/model/pay"
-    "github.com/flipped-aurora/gin-vue-admin/server/model/common/request"
-    payReq "github.com/flipped-aurora/gin-vue-admin/server/model/pay/request"
-    "github.com/flipped-aurora/gin-vue-admin/server/model/common/response"
-    "github.com/flipped-aurora/gin-vue-admin/server/service"
-    "github.com/gin-gonic/gin"
-    "go.uber.org/zap"
-    "github.com/flipped-aurora/gin-vue-admin/server/utils"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/common/request"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/common/response"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/pay"
+	payReq "github.com/flipped-aurora/gin-vue-admin/server/model/pay/request"
+	"github.com/flipped-aurora/gin-vue-admin/server/service"
+	"github.com/flipped-aurora/gin-vue-admin/server/utils"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type PaysApi struct {
 }
 
 var paysService = service.ServiceGroupApp.PayServiceGroup.PaysService
-
 
 // CreatePays 创建zmPay表
 // @Tags Pays
@@ -34,18 +33,21 @@ func (paysApi *PaysApi) CreatePays(c *gin.Context) {
 		response.FailWithMessage(err.Error(), c)
 		return
 	}
-    verify := utils.Rules{
-        "Name":{utils.NotEmpty()},
-        "CPrice":{utils.NotEmpty()},
-        "Number":{utils.NotEmpty()},
-        "Type":{utils.NotEmpty()},
-    }
+	verify := utils.Rules{
+		"Name":    {utils.NotEmpty()},
+		"CPrice":  {utils.NotEmpty()},
+		"Number":  {utils.NotEmpty()},
+		"Sort":    {utils.NotEmpty()},
+		"Checked": {utils.NotEmpty()},
+		"Status":  {utils.NotEmpty()},
+		"Type":    {utils.NotEmpty()},
+	}
 	if err := utils.Verify(pays, verify); err != nil {
-    		response.FailWithMessage(err.Error(), c)
-    		return
-    	}
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
 	if err := paysService.CreatePays(&pays); err != nil {
-        global.GVA_LOG.Error("创建失败!", zap.Error(err))
+		global.GVA_LOG.Error("创建失败!", zap.Error(err))
 		response.FailWithMessage("创建失败", c)
 	} else {
 		response.OkWithMessage("创建成功", c)
@@ -69,7 +71,7 @@ func (paysApi *PaysApi) DeletePays(c *gin.Context) {
 		return
 	}
 	if err := paysService.DeletePays(pays); err != nil {
-        global.GVA_LOG.Error("删除失败!", zap.Error(err))
+		global.GVA_LOG.Error("删除失败!", zap.Error(err))
 		response.FailWithMessage("删除失败", c)
 	} else {
 		response.OkWithMessage("删除成功", c)
@@ -87,13 +89,13 @@ func (paysApi *PaysApi) DeletePays(c *gin.Context) {
 // @Router /pays/deletePaysByIds [delete]
 func (paysApi *PaysApi) DeletePaysByIds(c *gin.Context) {
 	var IDS request.IdsReq
-    err := c.ShouldBindJSON(&IDS)
+	err := c.ShouldBindJSON(&IDS)
 	if err != nil {
 		response.FailWithMessage(err.Error(), c)
 		return
 	}
 	if err := paysService.DeletePaysByIds(IDS); err != nil {
-        global.GVA_LOG.Error("批量删除失败!", zap.Error(err))
+		global.GVA_LOG.Error("批量删除失败!", zap.Error(err))
 		response.FailWithMessage("批量删除失败", c)
 	} else {
 		response.OkWithMessage("批量删除成功", c)
@@ -116,18 +118,21 @@ func (paysApi *PaysApi) UpdatePays(c *gin.Context) {
 		response.FailWithMessage(err.Error(), c)
 		return
 	}
-      verify := utils.Rules{
-          "Name":{utils.NotEmpty()},
-          "CPrice":{utils.NotEmpty()},
-          "Number":{utils.NotEmpty()},
-          "Type":{utils.NotEmpty()},
-      }
-    if err := utils.Verify(pays, verify); err != nil {
-      	response.FailWithMessage(err.Error(), c)
-      	return
-     }
+	verify := utils.Rules{
+		"Name":    {utils.NotEmpty()},
+		"CPrice":  {utils.NotEmpty()},
+		"Number":  {utils.NotEmpty()},
+		"Sort":    {utils.NotEmpty()},
+		"Checked": {utils.NotEmpty()},
+		"Status":  {utils.NotEmpty()},
+		"Type":    {utils.NotEmpty()},
+	}
+	if err := utils.Verify(pays, verify); err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
 	if err := paysService.UpdatePays(pays); err != nil {
-        global.GVA_LOG.Error("更新失败!", zap.Error(err))
+		global.GVA_LOG.Error("更新失败!", zap.Error(err))
 		response.FailWithMessage("更新失败", c)
 	} else {
 		response.OkWithMessage("更新成功", c)
@@ -151,7 +156,7 @@ func (paysApi *PaysApi) FindPays(c *gin.Context) {
 		return
 	}
 	if repays, err := paysService.GetPays(pays.ID); err != nil {
-        global.GVA_LOG.Error("查询失败!", zap.Error(err))
+		global.GVA_LOG.Error("查询失败!", zap.Error(err))
 		response.FailWithMessage("查询失败", c)
 	} else {
 		response.OkWithData(gin.H{"repays": repays}, c)
@@ -175,14 +180,14 @@ func (paysApi *PaysApi) GetPaysList(c *gin.Context) {
 		return
 	}
 	if list, total, err := paysService.GetPaysInfoList(pageInfo); err != nil {
-	    global.GVA_LOG.Error("获取失败!", zap.Error(err))
-        response.FailWithMessage("获取失败", c)
-    } else {
-        response.OkWithDetailed(response.PageResult{
-            List:     list,
-            Total:    total,
-            Page:     pageInfo.Page,
-            PageSize: pageInfo.PageSize,
-        }, "获取成功", c)
-    }
+		global.GVA_LOG.Error("获取失败!", zap.Error(err))
+		response.FailWithMessage("获取失败", c)
+	} else {
+		response.OkWithDetailed(response.PageResult{
+			List:     list,
+			Total:    total,
+			Page:     pageInfo.Page,
+			PageSize: pageInfo.PageSize,
+		}, "获取成功", c)
+	}
 }
--- model/pay/pays.go
+++ model/pay/pays.go
@@ -3,24 +3,23 @@ package pay
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
-	
-	
 )
 
 // zmPay表 结构体  Pays
 type Pays struct {
-      global.GVA_MODEL
-      Name  string `json:"name" form:"name" gorm:"column:name;comment:名称;size:256;"`  //名称 
-      CPrice  *float64 `json:"cPrice" form:"cPrice" gorm:"column:c_price;comment:现价;size:10;"`  //现价 
-      OPrice  *float64 `json:"oPrice" form:"oPrice" gorm:"column:o_price;comment:原价;size:10;"`  //原价 
-      Number  *int `json:"number" form:"number" gorm:"column:number;comment:有效天数;size:10;"`  //有效天数 
-      NumberExt  *int `json:"numberExt" form:"numberExt" gorm:"column:number_ext;comment:赠送天数;size:10;"`  //赠送天数 
-      Type  string `json:"type" form:"type" gorm:"column:type;type:enum(256);comment:类型,1付费,2积分;"`  //类型,1付费,2积分 
+	global.GVA_MODEL
+	Name      string   `json:"name" form:"name" gorm:"column:name;comment:名称;size:256;"`                  //名称
+	CPrice    *float64 `json:"cPrice" form:"cPrice" gorm:"column:c_price;comment:现价;size:10;"`            //现价
+	OPrice    *float64 `json:"oPrice" form:"oPrice" gorm:"column:o_price;comment:原价;size:10;"`            //原价
+	Number    *int     `json:"number" form:"number" gorm:"column:number;comment:有效天数;size:10;"`           //有效天数
+	NumberExt *int     `json:"numberExt" form:"numberExt" gorm:"column:number_ext;comment:赠送天数;size:10;"` //赠送天数
+	Sort      *int     `json:"sort" form:"sort" gorm:"column:sort;comment:排序;size:10;"`
+	Checked   *bool    `json:"checked" form:"checked" gorm:"column:checked;comment:默认状态;size:10;"`     //默认状态
+	Status    *bool    `json:"status" form:"status" gorm:"column:status;comment:启用状态;size:10;"`        //启用状态
+	Type      string   `json:"type" form:"type" gorm:"column:type;type:enum(256);comment:类型,1付费,2积分;"` //类型,1付费,2积分
 }
-
 
 // TableName zmPay表 Pays自定义表名 zm_pay
 func (Pays) TableName() string {
-  return "zm_pay"
+	return "zm_pay"
 }
-
