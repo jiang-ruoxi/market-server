7199cdc1a073d0c343fea2506d5f3d77d7e59e65
若熙~ <oncwnuE9cvc3j9g4Yk0AFSNyIEF0@git.weixin.qq.com>
2024-04-01 22:58:32 +0800

    任务

--- model/bad/bad.go
+++ model/bad/bad.go
@@ -1,21 +1,17 @@
 // 自动生成模板BadWords
 package bad
 
-import (
-	"github.com/flipped-aurora/gin-vue-admin/server/global"
-	
-	
-)
+import "time"
 
 // bad 结构体  BadWords
 type BadWords struct {
-      global.GVA_MODEL
-      Name  string `json:"name" form:"name" gorm:"column:name;comment:敏感词;size:255;"`  //敏感词 
+	ID        int       `gorm:"primarykey" json:"ID"`                                      // 主键ID
+	Name      string    `json:"name" form:"name" gorm:"column:name;comment:敏感词;size:255;"` //敏感词
+	CreatedAt time.Time // 创建时间
+	IsDeleted int       `json:"is_deleted" form:"is_deleted" gorm:"column:is_deleted;comment:是否删除,1已删除,0正常;"` //是否删除,1已删除,0正常
 }
-
 
 // TableName bad BadWords自定义表名 zm_bad_words
 func (BadWords) TableName() string {
-  return "zm_bad_words"
+	return "zm_bad_words"
 }
-
--- service/bad/bad.go
+++ service/bad/bad.go
@@ -3,60 +3,52 @@ package bad
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/bad"
+	badReq "github.com/flipped-aurora/gin-vue-admin/server/model/bad/request"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/common/request"
-    badReq "github.com/flipped-aurora/gin-vue-admin/server/model/bad/request"
 )
 
 type BadWordsService struct {
 }
 
 // CreateBadWords 创建bad记录
-// Author [piexlmax](https://github.com/piexlmax)
 func (badWordsService *BadWordsService) CreateBadWords(badWords *bad.BadWords) (err error) {
 	err = global.MustGetGlobalDBByDBName("market").Create(badWords).Error
 	return err
 }
 
 // DeleteBadWords 删除bad记录
-// Author [piexlmax](https://github.com/piexlmax)
 func (badWordsService *BadWordsService)DeleteBadWords(badWords bad.BadWords) (err error) {
-	err = global.MustGetGlobalDBByDBName("market").Delete(&badWords).Error
+	var s bad.BadWords
+	err = global.MustGetGlobalDBByDBName("market").Model(&s).Debug().Where("id=?", badWords.ID).Update("is_deleted", 1).Error
 	return err
 }
 
 // DeleteBadWordsByIds 批量删除bad记录
-// Author [piexlmax](https://github.com/piexlmax)
 func (badWordsService *BadWordsService)DeleteBadWordsByIds(ids request.IdsReq) (err error) {
-	err = global.MustGetGlobalDBByDBName("market").Delete(&[]bad.BadWords{},"id in ?",ids.Ids).Error
+	var s bad.BadWords
+	err = global.MustGetGlobalDBByDBName("market").Model(&s).Debug().Where("id IN ?", ids.Ids).Updates(&bad.BadWords{IsDeleted: 1}).Error
 	return err
 }
 
 // UpdateBadWords 更新bad记录
-// Author [piexlmax](https://github.com/piexlmax)
 func (badWordsService *BadWordsService)UpdateBadWords(badWords bad.BadWords) (err error) {
 	err = global.MustGetGlobalDBByDBName("market").Save(&badWords).Error
 	return err
 }
 
 // GetBadWords 根据id获取bad记录
-// Author [piexlmax](https://github.com/piexlmax)
-func (badWordsService *BadWordsService)GetBadWords(id uint) (badWords bad.BadWords, err error) {
+func (badWordsService *BadWordsService)GetBadWords(id int) (badWords bad.BadWords, err error) {
 	err = global.MustGetGlobalDBByDBName("market").Where("id = ?", id).First(&badWords).Error
 	return
 }
 
 // GetBadWordsInfoList 分页获取bad记录
-// Author [piexlmax](https://github.com/piexlmax)
 func (badWordsService *BadWordsService)GetBadWordsInfoList(info badReq.BadWordsSearch) (list []bad.BadWords, total int64, err error) {
 	limit := info.PageSize
 	offset := info.PageSize * (info.Page - 1)
     // 创建db
-	db := global.MustGetGlobalDBByDBName("market").Model(&bad.BadWords{})
+	db := global.MustGetGlobalDBByDBName("market").Model(&bad.BadWords{}).Where("is_deleted = 0")
     var badWordss []bad.BadWords
-    // 如果有条件搜索 下方会自动创建搜索语句
-    if info.StartCreatedAt !=nil && info.EndCreatedAt !=nil {
-     db = db.Where("created_at BETWEEN ? AND ?", info.StartCreatedAt, info.EndCreatedAt)
-    }
 	err = db.Count(&total).Error
 	if err!=nil {
     	return
