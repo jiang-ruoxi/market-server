f11683e99c4ceb592a106cc63a5997d9454b3c7a
若熙~ <oncwnuE9cvc3j9g4Yk0AFSNyIEF0@git.weixin.qq.com>
2024-04-03 23:14:14 +0800

    修

--- model/address/address.go
+++ model/address/address.go
@@ -2,14 +2,17 @@
 package address
 
 import (
-	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"time"
 )
 
 // zmAddress表 结构体  Address
 type Address struct {
-	global.GVA_MODEL
-	Name string `json:"name" form:"name" gorm:"column:name;comment:城市;size:255;"` //城市
-	Sort int    `json:"sort" form:"sort" gorm:"column:sort;comment:排序,倒序;"`
+	ID        int       `gorm:"primarykey" json:"ID"` // 主键ID
+	CreatedAt time.Time // 创建时间
+	UpdatedAt time.Time // 更新时间
+	Name      string    `json:"name" form:"name" gorm:"column:name;comment:城市;size:255;"` //城市
+	Sort      int       `json:"sort" form:"sort" gorm:"column:sort;comment:排序,倒序;"`
+	IsDeleted int       `json:"is_deleted" form:"is_deleted" gorm:"column:is_deleted;comment:是否删除,1已删除,0正常;"` //是否删除,1已删除,0正常
 }
 
 // TableName zmAddress表 Address自定义表名 zm_address
--- service/address/address.go
+++ service/address/address.go
@@ -3,69 +3,65 @@ package address
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/address"
+	addressReq "github.com/flipped-aurora/gin-vue-admin/server/model/address/request"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/common/request"
-    addressReq "github.com/flipped-aurora/gin-vue-admin/server/model/address/request"
 )
 
 type AddressService struct {
 }
 
 // CreateAddress 创建zmAddress表记录
-// Author [piexlmax](https://github.com/piexlmax)
 func (zmAddressService *AddressService) CreateAddress(zmAddress *address.Address) (err error) {
 	err = global.MustGetGlobalDBByDBName("market").Create(zmAddress).Error
 	return err
 }
 
 // DeleteAddress 删除zmAddress表记录
-// Author [piexlmax](https://github.com/piexlmax)
-func (zmAddressService *AddressService)DeleteAddress(zmAddress address.Address) (err error) {
-	err = global.MustGetGlobalDBByDBName("market").Delete(&zmAddress).Error
+func (zmAddressService *AddressService) DeleteAddress(zmAddress address.Address) (err error) {
+	var s address.Address
+	err = global.MustGetGlobalDBByDBName("market").Model(&s).Debug().Where("id=?", zmAddress.ID).Update("is_deleted", 1).Error
 	return err
 }
 
 // DeleteAddressByIds 批量删除zmAddress表记录
-// Author [piexlmax](https://github.com/piexlmax)
-func (zmAddressService *AddressService)DeleteAddressByIds(ids request.IdsReq) (err error) {
-	err = global.MustGetGlobalDBByDBName("market").Delete(&[]address.Address{},"id in ?",ids.Ids).Error
+func (zmAddressService *AddressService) DeleteAddressByIds(ids request.IdsReq) (err error) {
+	var s address.Address
+	err = global.MustGetGlobalDBByDBName("market").Model(&s).Debug().Where("id IN ?", ids.Ids).Updates(&address.Address{IsDeleted: 1}).Error
 	return err
 }
 
 // UpdateAddress 更新zmAddress表记录
-// Author [piexlmax](https://github.com/piexlmax)
-func (zmAddressService *AddressService)UpdateAddress(zmAddress address.Address) (err error) {
+func (zmAddressService *AddressService) UpdateAddress(zmAddress address.Address) (err error) {
 	err = global.MustGetGlobalDBByDBName("market").Save(&zmAddress).Error
 	return err
 }
 
 // GetAddress 根据id获取zmAddress表记录
-// Author [piexlmax](https://github.com/piexlmax)
-func (zmAddressService *AddressService)GetAddress(id uint) (zmAddress address.Address, err error) {
+func (zmAddressService *AddressService) GetAddress(id int) (zmAddress address.Address, err error) {
 	err = global.MustGetGlobalDBByDBName("market").Where("id = ?", id).First(&zmAddress).Error
 	return
 }
 
 // GetAddressInfoList 分页获取zmAddress表记录
-// Author [piexlmax](https://github.com/piexlmax)
-func (zmAddressService *AddressService)GetAddressInfoList(info addressReq.AddressSearch) (list []address.Address, total int64, err error) {
+func (zmAddressService *AddressService) GetAddressInfoList(info addressReq.AddressSearch) (list []address.Address, total int64, err error) {
 	limit := info.PageSize
 	offset := info.PageSize * (info.Page - 1)
-    // 创建db
-	db := global.MustGetGlobalDBByDBName("market").Model(&address.Address{})
-    var zmAddresss []address.Address
-    // 如果有条件搜索 下方会自动创建搜索语句
-    if info.StartCreatedAt !=nil && info.EndCreatedAt !=nil {
-     db = db.Where("created_at BETWEEN ? AND ?", info.StartCreatedAt, info.EndCreatedAt)
-    }
+	// 创建db
+	db := global.MustGetGlobalDBByDBName("market").Model(&address.Address{}).Where("is_deleted=0")
+	var zmAddresss []address.Address
+	// 如果有条件搜索 下方会自动创建搜索语句
+	if info.StartCreatedAt != nil && info.EndCreatedAt != nil {
+		db = db.Where("created_at BETWEEN ? AND ?", info.StartCreatedAt, info.EndCreatedAt)
+	}
 	err = db.Count(&total).Error
-	if err!=nil {
-    	return
-    }
+	if err != nil {
+		return
+	}
 
 	if limit != 0 {
-       db = db.Limit(limit).Offset(offset)
-    }
-	
+		db = db.Limit(limit).Offset(offset)
+	}
+
 	err = db.Find(&zmAddresss).Error
-	return  zmAddresss, total, err
+	return zmAddresss, total, err
 }
