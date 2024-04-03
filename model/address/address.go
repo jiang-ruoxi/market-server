8643e44c5d4ed0ac16726d8521350cc4cc89eeaa
若熙~ <oncwnuE9cvc3j9g4Yk0AFSNyIEF0@git.weixin.qq.com>
2024-04-03 22:57:45 +0800

    增加自动

--- model/address/address.go
+++ model/address/address.go
@@ -3,19 +3,16 @@ package address
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
-	
-	
 )
 
 // zmAddress表 结构体  Address
 type Address struct {
-      global.GVA_MODEL
-      Name  string `json:"name" form:"name" gorm:"column:name;comment:城市;size:255;"`  //城市 
+	global.GVA_MODEL
+	Name string `json:"name" form:"name" gorm:"column:name;comment:城市;size:255;"` //城市
+	Sort int    `json:"sort" form:"sort" gorm:"column:sort;comment:排序,倒序;"`
 }
-
 
 // TableName zmAddress表 Address自定义表名 zm_address
 func (Address) TableName() string {
-  return "zm_address"
+	return "zm_address"
 }
-
